e94c254ae2920822822019efa2153304b7e7f7c3
Mohammed <gsft22221@gmail.com>
2021-11-01 19:16:53 +0200

    Fix deepsource issue

--- src/components/PhoneUI/ChatScreen/index.js
+++ src/components/PhoneUI/ChatScreen/index.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from "react";
+import React, { useState } from 'react';
 import { auth, firestore } from "../../../firebase";
 import ChatInput from "./ChatInput";
 import Messages from "./Messages";
